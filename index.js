c8161665a076b008d3ac1524d89902a13195dad9
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-07-02 23:57:02 +0200

    deleted passport on get all movies

--- index.js
+++ index.js
@@ -52,20 +52,16 @@ app.get("/documentation", (req, res) => {
 });
 
 //Display all movies
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 // Get one movie, by title
 app.get("/movies/:Title", (req, res) => {
