e15f63e27b1201b7b2c3e72f1a29a3c05c2f599b
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-07-02 11:13:07 +0200

    2.9 Added auth.js and passport.js
    Added authentification, added auth.js and passport.js, integrated necessary commands in index.js

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(
   })
 );
 
+let auth = require("./auth")(app);
+const passport = require("passport");
+require("./passport");
+
 // Connect to MongoDB
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -48,73 +52,93 @@ app.get("/documentation", (req, res) => {
 });
 
 //Display all movies
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Get one movie, by title
-app.get("/movies/:Title", (req, res) => {
-  Movies.findOne(
-    { Title: req.params.Title },
-    { Title: 1, Description: 1, _id: 0 }
-  )
-    .then((movie) => {
-      res.json(movie);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies/:Title",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.findOne(
+      { Title: req.params.Title },
+      { Title: 1, Description: 1, _id: 0 }
+    )
+      .then((movie) => {
+        res.json(movie);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Get movies by genre
-app.get("/movies/genre/:name", (req, res) => {
-  Movies.find(
-    { "Genre.Name": req.params.name },
-    { Title: 1, Description: 1, _id: 0 }
-  )
-    .then((movie) => {
-      res.status(201).json(movie);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies/genre/:name",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find(
+      { "Genre.Name": req.params.name },
+      { Title: 1, Description: 1, _id: 0 }
+    )
+      .then((movie) => {
+        res.status(201).json(movie);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Get info about director, by director's name
-app.get("/movies/director/:Name", (req, res) => {
-  Movies.findOne(
-    { "Director.Name": req.params.Name },
-    { Director: 1, Bio: 1, _id: 0 }
-  )
-    .then((director) => {
-      res.json(director);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies/director/:Name",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.findOne(
+      { "Director.Name": req.params.Name },
+      { Director: 1, Bio: 1, _id: 0 }
+    )
+      .then((director) => {
+        res.json(director);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //Get all users
-app.get("/users", (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.find()
+      .then((users) => {
+        res.status(201).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //Create new user
 /* JSON expected in this format
