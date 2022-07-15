d159d41b4d72029f6f12ed2f5f254f65b1fc2c02
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-07-15 12:09:43 +0200

    temporarily stop auth for movies

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/documentation", (req, res) => {
 //Display all movies
 app.get(
   "/movies",
-  passport.authenticate("jwt", { session: false }),
+  //passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
